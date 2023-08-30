c0f40bd1167e8ce2ace0c4f78e201f66c24b6d6d
aparna paladugu <aparnachowdary72837@gmail.com>
2023-08-30 10:01:36 +0530

    updated

--- src/TodoList.js
+++ src/TodoList.js
@@ -28,19 +28,3 @@ export default TodoList
 
 
 
-
-
-// import React from 'react'
-
-// const TodoList = ({todolist,deleteHandler}) => {
-//     return (
-//         <div>
-//             {todolist.map((todo,index) =>
-//             <div key={index}>
-//                 <h5>{todo} &nbsp; <button onClick={() => deleteHandler(index)}>Delete</button></h5>
-//             </div>)}
-//         </div>
-//     )
-// }
-
-// export default TodoList
